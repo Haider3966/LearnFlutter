365314b821d6e940373848ff5e0e8d56871b1277
Haider3966 <ashrafhaider143@gmail.com.com>
2023-10-26 15:13:55 +0600

    Mediaquery

--- lib/main.dart
+++ lib/main.dart
@@ -5,48 +5,20 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Row"),
-        ),
-        body:ListView(
-          children: [
-            Column(
-              children: <Widget>[
-                Container(
-                  height: 200,
-                  width: 150,
-                  color: Colors.deepOrange,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  height: 200,
-                  width: 150,
-                  color: Colors.deepOrange,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  height: 200,
-                  width: 150,
-                  color: Colors.deepOrange,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  height: 200,
-                  width: 150,
-                  color: Colors.deepOrange,
-                ),
-              ],
+      home:SafeArea(
+        child: SafeArea(
+          child: Scaffold(
+            body: Center(
+              child: Container(
+                height: MediaQuery.of(context).size.height/3,
+                width: MediaQuery.of(context).size.width/2,
+                color: Colors.deepOrange,
+              ),
             ),
-          ],
+          ) ,
         ),
       ),
     );
