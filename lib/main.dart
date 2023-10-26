a0fd2c7cb10ae776ee354556573be0de24f0d056
Haider3966 <ashrafhaider143@gmail.com.com>
2023-10-26 15:18:48 +0600

    Mediaquery2

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,8 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
+   var screenheight= MediaQuery.of(context).size.height;
+   var containerheight= screenheight/4;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home:SafeArea(
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
           child: Scaffold(
             body: Center(
               child: Container(
-                height: MediaQuery.of(context).size.height/3,
+                height: containerheight  ,
                 width: MediaQuery.of(context).size.width/2,
                 color: Colors.deepOrange,
               ),
