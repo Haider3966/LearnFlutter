008a5850059e8b769c1c7bc07ad8621e807ee8a1
Haider3966 <ashrafhaider143@gmail.com.com>
2023-10-27 09:27:28 +0600

    Stack,Positioned ,Floating action button

--- lib/main.dart
+++ lib/main.dart
@@ -5,32 +5,27 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:Scaffold(
-        body: ListView(
-          children: [
-            ListTile(
-              title: Text("Easy Explation"),
-              subtitle: Text("Learn everything with easy explanation"),
-              leading: CircleAvatar(child: Icon(Icons.message),),
-              trailing: Icon(Icons.add_a_photo),
-            ),
-            ListTile(
-              title: Text("Easy Explation"),
-              subtitle: Text("Learn everything with easy explanation"),
-              leading: CircleAvatar(child: Icon(Icons.message),),
-              trailing: Icon(Icons.add_a_photo),
-            ),
-            ListTile(
-              title: Text("Easy Explation"),
-              subtitle: Text("Learn everything with easy explanation"),
-              leading: CircleAvatar(child: Icon(Icons.message),),
-              trailing: Icon(Icons.add_a_photo),
-            ),
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(onPressed: (){},child:Icon(Icons.add),),
+        body: Center(
+          child: Stack(
+            alignment: Alignment.bottomCenter,
 
-          ],
+            children: <Widget>[
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.amber,
+              ),
+              Positioned(
+                bottom: -50,
+                  child: CircleAvatar(
+                radius: 50,
+              ))
+            ],
+          ),
         ),
       ),
     );
